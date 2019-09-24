abb8984d03a1210f1e408e7a703384fdfb5f0698
M Arvan Nurcahyana <55648161+arvannc@users.noreply.github.com>
2019-09-24 18:34:47 +0700

    Add files via upload

---
+++ src/if1/pkg10118021/latihan10/logika/IF110118021Latihan10Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118021.latihan10.logika;
+
+/**
+ *
+ * @author Arvan
+ * NAMA     : M Arvan Nurcahyana
+ * KELAS    : IF-1
+ * NIM      : 10118021
+ * Deskripsi Program : Logika
+ */
+public class IF110118021Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status dewasa : " + dewasa);
+    }
+    
+}
